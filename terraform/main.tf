46ba85162b057424ea5c9247c5918045f2997120
viniap <vinicius.peres@vlgi.com.br>
2024-03-02 00:33:52 -0300

    sns_topic (feat): create policy to allow publishing to the topic

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,22 @@
-resource "aws_sns_topic" "pluggy_webhook_events_topic" {
+resource "aws_sns_topic" "topic" {
   name = "${var.sns_topic_name}"
   kms_master_key_id = "alias/aws/sns"
+}
+
+resource "aws_iam_policy" "publish_topic_policy" {
+  name        = "AmazonSNSPublish${var.sns_topic_name}Policy"
+  description = "Allows to publish to the ${var.sns_topic_name} SNS topic."
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sns:Publish*",
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_sns_topic.topic.arn}"
+      },
+    ]
+  })
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "sns_topic_arn" {
-  value = aws_sns_topic.pluggy_webhook_events_topic.arn
+  value = aws_sns_topic.topic.arn
+}
+
+output "publish_topic_policy_arn" {
+  value = aws_iam_policy.publish_topic_policy.arn
 }
