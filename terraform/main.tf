223322ee110c5e9492bc9ebd7df5e0ecd0108bcf
viniaperes <viniciusgalego8@live.com>
2024-03-03 15:23:03 -0300

    api_gateway (feat): create api_gateway role and attach sns policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,27 @@ resource "aws_iam_policy" "publish_topic_policy" {
     ]
   })
 }
+
+resource "aws_iam_role" "api_gateway_sns_proxy_role" {
+  name = "APIGatewaySNSProxyRoleTest"
+  description = "Allows API Gateway to interact with SNS topics."
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_publish_topic_policy" {
+  role       = aws_iam_role.api_gateway_sns_proxy_role.name
+  policy_arn = aws_iam_policy.publish_topic_policy.arn
+}
